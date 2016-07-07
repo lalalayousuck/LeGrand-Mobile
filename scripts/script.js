76955c6167c7f22a9ac05bfc628d6fcfcd667152
lalalayousuck <tonyfirehead@gmail.com>
2016-07-07 10:45:53 +0300

    iter5

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,15 @@
 $(document).ready(function(){
   $('.flexslider').flexslider({
-    animation: "slide"
+    animation: "slide",
+    directionNav: false
   });
+
+  $('.p-switchers-item').click(function(){
+  	$('.p-gallery').hide().flexslider('stop');
+  	var galleryIndex = $(this).index();
+  	console.log(galleryIndex);
+  	$(".p-gallery-" + (galleryIndex + 1)).show();
+  });
+
+
 });
