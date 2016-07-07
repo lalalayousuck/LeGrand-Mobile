1ac36bc730a3c24ca3cef97949d5f1149ff808b3
lalalayousuck <tonyfirehead@gmail.com>
2016-07-07 13:22:52 +0300

    markup final

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,16 @@
 $(document).ready(function(){
+
+
   $('.flexslider').flexslider({
     animation: "slide",
-    directionNav: false
+    directionNav: false,
   });
 
   $('.p-switchers-item').click(function(){
-  	$('.p-gallery').hide().flexslider('stop');
+  	$('.p-gallery').css({'opacity': 0, 'visibility': 'hidden'});
   	var galleryIndex = $(this).index();
   	console.log(galleryIndex);
-  	$(".p-gallery-" + (galleryIndex + 1)).show();
+  	$(".p-gallery-" + (galleryIndex + 1)).css({'opacity': 1, 'visibility': 'visible'});
   });
 
 
